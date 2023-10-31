fa8e63ab1ca9ead1554b51d1bdac9ed3ebf0efac
Your Name <tha1242002@gmail.com>
2023-10-31 11:10:30 +0700

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,43 @@
+function load(selector, path) {
+    const cached = localStorage.getItem(path);
+    if (cached) {
+        document.querySelector(selector).innerHTML = cached;
+    }
+
+    fetch(path)
+        .then((res) => res.text())
+        .then((html) => {
+            if (html !== cached) {
+                document.querySelector(selector).innerHTML = html;
+                localStorage.setItem(path, html);
+            }
+        });
+}
+/**
+ * JS toggle
+ *
+ * Cách dùng:
+ * <button class="js-toggle" toggle-target="#box">Click</button>
+ * <div id="box">Content show/hide</div>
+ */
+window.addEventListener("template-loaded", initJsToggle);
+
+function initJsToggle() {
+    $$(".js-toggle").forEach((button) => {
+        const target = button.getAttribute("toggle-target");
+        if (!target) {
+            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+        }
+        button.onclick = () => {
+            if (!$(target)) {
+                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+            }
+            const isHidden = $(target).classList.contains("hide");
+
+            requestAnimationFrame(() => {
+                $(target).classList.toggle("hide", !isHidden);
+                $(target).classList.toggle("show", isHidden);
+            });
+        };
+    });
+}
